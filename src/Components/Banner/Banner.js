5cd681caeca4896f0fb386d58f4a2b3ccb6ca5fe
basim <basims0077@gmail.com>
2023-01-18 11:33:51 +0530

    update

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -6,13 +6,13 @@ const Banner = () => {
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="https://img.freepik.com/premium-vector/black-friday-sale-concept-design_118124-4573.jpg" class="d-block w-100" alt="..." />
+                        <img src="image/b1.jpg" class="d-block w-100 " alt="..." />
                     </div>
                     <div class="carousel-item">
-                        <img src="https://img.freepik.com/premium-vector/black-friday-sale-concept-design_118124-4573.jpg" class="d-block w-100" alt="..." />
+                        <img src="image/b2 (2).jpg" class="d-block w-100" alt="..." />
                     </div>
                     <div class="carousel-item">
-                        <img src="https://img.freepik.com/premium-vector/black-friday-sale-concept-design_118124-4573.jpg" class="d-block w-100" alt="..." />
+                        <img src="image/b3.jpg" class="d-block w-100" alt="..." />
                     </div>
                 </div>
             </div>
